dea372806b732f49a0ff27720e477d0018ef64e6
DoyeonAce <doyeonace@gmail.com>
2024-07-22 13:49:48 +0900

    baekjoon c_study

---
+++ c_study/problem10926_test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+    char num[10];
+    scanf("%s", num);
+    printf("%s\n", num);
+    return 0;
+
+}
---
+++ c_study/problem18108.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    int year;
+    scanf("%d", &year);
+    printf("%d\n", year-453);
+    return 0;
+}
