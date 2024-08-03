4162d236ac2f52dfed37a327e97c8c3e349bad74
DoyeonAce <doyeonace@gmail.com>
2024-08-03 16:56:01 +0900

    stdudying by baekjoon

---
+++ c_study/problem2525.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (void)
+{
+    int a, b, c;
+    scanf("%d %d ", &a, &b);
+    scanf("%d", &c);
+    if (b+c >= 60)
+    {
+        a = a+((b+c)/60);
+        b = (b+c) % 60;
+        if (a>=24)
+        {
+            a = a%24;
+        }
+    }
+    else
+    {
+        b = b+c;
+    }
+    printf("%d %d", a, b);
+    return 0;
+}
